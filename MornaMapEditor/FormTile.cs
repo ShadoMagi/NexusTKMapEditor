fc453f1e1cb85bf379fa8856e1b359635e234764
Scott Greene <ShadoMagi@gmail.com>
2020-05-23 14:08:42 -0700

    Only draw the visible tiles again, also re-enable grid drawing.

--- MornaMapEditor/FormTile.cs
+++ MornaMapEditor/FormTile.cs
@@ -9,11 +9,13 @@ namespace MornaMapEditor
     {
 
         private static readonly int pixelBuffer = 10;
-        private Image fullTileRendering;
         private static readonly FormTile FormInstance = new FormTile();
 
         private readonly List<Point> selectedTiles = new List<Point>();
         private Point focusedTile = new Point(-1,-1);
+        private int usableHeight = 0;
+        private int tilesPerRow = 0;
+        private int tileRows = 0;
         private int sizeModifier;
         private bool showGrid;
         
@@ -32,58 +34,24 @@ namespace MornaMapEditor
         {
             InitializeComponent();
             sizeModifier = ImageRenderer.Singleton.sizeModifier;
-            this.MouseWheel += new MouseEventHandler(frmTile_MouseWheel);
-            //this.Paint += frmTile_Paint;
+            MouseWheel += frmTile_MouseWheel;
         }
 
         private void frmTile_Load(object sender, EventArgs e)
         {
             menuStrip.Visible = false;
             MinimumSize = new Size(sizeModifier + pixelBuffer, sizeModifier + sb1.Height + menuStrip.Height + statusStrip.Height + pixelBuffer);
-            RenderTileset();
+            updateTileWindow();
         }
 
-        private void RenderTileset()
+        private void updateTileWindow()
         {
-            if(fullTileRendering != null)
-                fullTileRendering.Dispose();
-            if (WindowState == FormWindowState.Minimized)
-                return;
-
-            //Bitmap tSet = new Bitmap(360, 360);
-            int usableHeight = Height - sb1.Height - menuStrip.Height - statusStrip.Height - pixelBuffer;
-            int tileRows = usableHeight / sizeModifier;
-            int tilesPerRow = TileManager.Epf[0].max / tileRows;
-            int currentRow = 0;
-            int currentColumn = 0;
-
-            sb1.Maximum = tilesPerRow;
+            usableHeight = Height - sb1.Height - menuStrip.Height - statusStrip.Height - pixelBuffer;
+            tileRows = usableHeight / sizeModifier;
+            tilesPerRow = TileManager.Epf[0].max / tileRows;
+            sb1.Maximum = tilesPerRow + (Width / sizeModifier);
             sb1.LargeChange = (Width / sizeModifier);
-            Bitmap tmpBitmap = new Bitmap(tilesPerRow * sizeModifier, usableHeight);
-            Graphics g = Graphics.FromImage(tmpBitmap);
-            g.Clear(Color.DarkGreen);
-
-            for (int tileNumber = 0; tileNumber < TileManager.Epf[0].max; tileNumber++)
-            {
-                int xPos =  currentColumn * sizeModifier;
-                int yPos = currentRow * sizeModifier;
-                g.DrawImage(ImageRenderer.Singleton.GetTileBitmap(tileNumber), xPos, yPos);
-
-                currentColumn++;
-                if (currentColumn > tilesPerRow)
-                {
-                    currentRow++;
-                    currentColumn = 0;
-                }
-            }
-
-            g.Dispose();
-            fullTileRendering = tmpBitmap;
             Invalidate();
-            //this.BackgroundImage = tSet;
-            //picTileset.Image = tSet;
-            //Application.DoEvents();
-            //tSet = null;
         }
 
         private void sb1_Scroll(object sender, ScrollEventArgs e)
@@ -96,12 +64,49 @@ namespace MornaMapEditor
         {
             if (BackgroundImage != null)
                 BackgroundImage.Dispose();
-            int usableHeight = Height - sb1.Height - menuStrip.Height - statusStrip.Height + pixelBuffer;
+            if (WindowState == FormWindowState.Minimized)
+                return;
+
+            //Bitmap tSet = new Bitmap(360, 360);
             Bitmap tmpBitmap = new Bitmap(Width, usableHeight);
             Graphics graphics = Graphics.FromImage(tmpBitmap);
+            graphics.Clear(Color.DarkGreen);
+            Pen penGrid = new Pen(Color.LightCyan, 1);
+            Pen penSelected = new Pen(Color.Red, 2);
+            Pen penFocused = new Pen(Color.Green, 2);
 
-            Rectangle sourceRectangle = new Rectangle(sb1.Value * sizeModifier,0,Width,usableHeight);
-            graphics.DrawImage(fullTileRendering, 0,0,sourceRectangle, GraphicsUnit.Pixel);
+            for (int xIndex = 0; xIndex <= (int) Math.Ceiling(Convert.ToDouble(Width / sizeModifier)); xIndex++)
+            {
+                for (int yIndex = 0; yIndex <= tileRows; yIndex++)
+                {
+                    Rectangle tileRectangle = new Rectangle(xIndex * sizeModifier, yIndex * sizeModifier, sizeModifier, sizeModifier);
+                    int tileNumber = (sb1.Value + xIndex) + (tilesPerRow * yIndex);
+                    if (tileNumber < TileManager.Epf[0].max)
+                    {
+                        graphics.DrawImage(ImageRenderer.Singleton.GetTileBitmap(tileNumber), tileRectangle);
+                    }
+                    
+                    if (ShowGrid)
+                    {
+                        e.Graphics.DrawRectangle(penGrid, tileRectangle);
+                            } 
+                    // if (selectedTiles.Count > 0)
+                    // {
+                    //
+                    //     foreach (var selectedTile in selectedTiles)
+                    //     {
+                    //         e.Graphics.DrawRectangle(penSelected, tileRectangle);
+                    //     }
+                    // }
+
+                    // if (focusedTile.X >= 0 && focusedTile.Y >= 0)
+                    // {
+                    //     e.Graphics.DrawRectangle(pen, focusedTile.X * sizeModifier, focusedTile.Y * sizeModifier, sizeModifier, sizeModifier);
+                    //     pen.Dispose();
+                    // }
+                }
+            }
+            
             BackgroundImage = tmpBitmap;
         }
 
@@ -119,57 +124,72 @@ namespace MornaMapEditor
             sb1_Scroll(null, null); 
         }
 
+        // private void frmTile_MouseMove(object sender, MouseEventArgs e)
+        // {
+        //     int newFocusedTileX = e.X / sizeModifier;
+        //     int newFocusedTileY = e.Y / sizeModifier;
+        //     bool refresh = (newFocusedTileX != focusedTile.X || newFocusedTileY != focusedTile.Y);
+        //
+        //     if (refresh)
+        //     {
+        //         focusedTile = new Point(newFocusedTileX, newFocusedTileY);
+        //         this.Invalidate();
+        //         int tileNumber = GetTileNumber(newFocusedTileX, newFocusedTileY);
+        //         toolStripStatusLabel.Text = string.Format("Tile number: {0}", tileNumber);
+        //     }
+        // }
+
         private void frmTile_MouseClick(object sender, MouseEventArgs e)
         {
-            if (e.Button != MouseButtons.Left) return;
-
-            int newSelectedTileX = e.X / sizeModifier;
-            int newSelectedTileY = e.Y / sizeModifier;
-
-            Point selectedTile = new Point(newSelectedTileX, newSelectedTileY);
-            if (ModifierKeys == Keys.Control)
-            {
-                if (!selectedTiles.Contains(selectedTile))
-                    selectedTiles.Add(selectedTile);
-            }
-            else
-            {
-                selectedTiles.Clear();
-                selectedTiles.Add(selectedTile);
-            }
-            TileManager.TileSelection = GetSelection();
-            TileManager.LastSelection = TileManager.SelectionType.Tile;
-            //RenderTileset();
+            // if (e.Button != MouseButtons.Left) return;
+            //
+            // int newSelectedTileX = e.X / sizeModifier;
+            // int newSelectedTileY = e.Y / sizeModifier;
+            //
+            // Point selectedTile = new Point(newSelectedTileX, newSelectedTileY);
+            // if (ModifierKeys == Keys.Control)
+            // {
+            //     if (!selectedTiles.Contains(selectedTile))
+            //         selectedTiles.Add(selectedTile);
+            // }
+            // else
+            // {
+            //     selectedTiles.Clear();
+            //     selectedTiles.Add(selectedTile);
+            // }
+            // TileManager.TileSelection = GetSelection();
+            // TileManager.LastSelection = TileManager.SelectionType.Tile;
+            // //RenderTileset();
         }
 
         public void AdjustSizeModifier(int newModifier)
         {
             sizeModifier = newModifier;
-            RenderTileset();
+            updateTileWindow();
             Invalidate();
         }
         
-        public Dictionary<Point, int> GetSelection()
-        {
-            Dictionary<Point, int> dictionary = new Dictionary<Point, int>();
-            if (selectedTiles.Count == 0) return dictionary;
-
-            int xMin = selectedTiles[0].X, yMin = selectedTiles[0].Y;
-
-            foreach (Point selectedTile in selectedTiles)
-            {
-                if (xMin > selectedTile.X) xMin = selectedTile.X;
-                if (yMin > selectedTile.Y) yMin = selectedTile.Y;
-            }
-
-            foreach (Point selectedTile in selectedTiles)
-            {
-                //dictionary.Add(new Point(selectedTile.X - xMin, selectedTile.Y - yMin), 
-                //    GetTileNumber(selectedTile.X, selectedTile.Y));
-            }
-
-            return dictionary;
-        }
+        // public Dictionary<Point, int> GetSelection()
+        // {
+        //     Dictionary<Point, int> dictionary = new Dictionary<Point, int>();
+        //     if (selectedTiles.Count == 0) return dictionary;
+        //
+        //     int xMin = selectedTiles[0].X, yMin = selectedTiles[0].Y;
+        //
+        //     foreach (Point selectedTile in selectedTiles)
+        //     {
+        //         if (xMin > selectedTile.X) xMin = selectedTile.X;
+        //         if (yMin > selectedTile.Y) yMin = selectedTile.Y;
+        //     }
+        //
+        //     foreach (Point selectedTile in selectedTiles)
+        //     {
+        //         dictionary.Add(new Point(selectedTile.X - xMin, selectedTile.Y - yMin), 
+        //             GetTileNumber(selectedTile.X, selectedTile.Y));
+        //     }
+        //
+        //     return dictionary;
+        // }
 
         /*public void ClearSelection()
         {
@@ -179,17 +199,33 @@ namespace MornaMapEditor
 
         private void findTileToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            NumberInputForm numberInputForm = new NumberInputForm(@"Enter object number");
-            if (numberInputForm.ShowDialog(this) == DialogResult.OK)
-            {
-               // NavigateToTile(numberInputForm.Number);
-            }
+        //     NumberInputForm numberInputForm = new NumberInputForm(@"Enter object number");
+        //     if (numberInputForm.ShowDialog(this) == DialogResult.OK)
+        //     {
+        //         NavigateToTile(numberInputForm.Number);
+        //     }
         }
 
         private void showGridToolStripMenuItem_Click(object sender, EventArgs e)
         {
             ShowGrid = showGridToolStripMenuItem.Checked;
         }
+        
+        // public void NavigateToTile(int number)
+        // {
+        //     if (number < 0 || number>(sb1.Maximum*100 + 99)) return;
+        //
+        //     int sbIndex = number / 100;
+        //     int y = (number - sbIndex * 100) / 10;
+        //     int x = number - sbIndex * 100 - y * 10;
+        //
+        //     sb1.Value = sbIndex;
+        //     selectedTiles.Clear();
+        //     selectedTiles.Add(new Point(x, y));
+        //     TileManager.TileSelection = GetSelection();
+        //     TileManager.LastSelection = TileManager.SelectionType.Tile;
+        //     RenderTileset();
+        // }
 
         private void FormTile_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -204,7 +240,7 @@ namespace MornaMapEditor
         private void FormTile_ResizeEnd(object sender, EventArgs e)
         {
             //MessageBox.Show("Got a ResizeEnd");
-            RenderTileset();
+            updateTileWindow();
         }
 
         private void FormTile_Move(object sender, EventArgs e)
